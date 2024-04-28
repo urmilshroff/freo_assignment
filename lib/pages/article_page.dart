d17e5e893003defe30cc59994ae5aaa1a06c1c7c
urmilshroff <urmilshroff@gmail.com>
2024-04-28 16:44:51 +0530

    feat: open wiki link in browser

--- lib/pages/article_page.dart
+++ lib/pages/article_page.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 import 'package:freo_assignment/providers/article_info_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ArticlePage extends ConsumerWidget {
   final String articleName;
+  final wikiLink = 'https://en.wikipedia.org/wiki/';
 
   const ArticlePage({super.key, required this.articleName});
 
@@ -12,7 +14,19 @@ class ArticlePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final articleInfoWatcher = ref.watch(articleInfoProvider(articleName));
     return Scaffold(
-      appBar: AppBar(title: Text(articleName)),
+      appBar: AppBar(
+        title: Text(articleName),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              if (!await launchUrl(Uri.parse(wikiLink + articleName))) {
+                throw Exception();
+              }
+            },
+            icon: Icon(Icons.open_in_browser_rounded),
+          ),
+        ],
+      ),
       body: articleInfoWatcher.when(
         data: (articleInfo) => SingleChildScrollView(
           padding: EdgeInsets.all(8.0),
